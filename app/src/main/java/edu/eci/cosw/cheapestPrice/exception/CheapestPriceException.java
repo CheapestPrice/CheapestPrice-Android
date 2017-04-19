f4b5e6233fdf5bc14fc32719079a81540486336c
Daniela <daniela.sepulveda@mail.escuelaing.edu.co>
2017-04-19 17:15:59 -0500

    first commit

---
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/exception/CheapestPriceException.java
@@ -0,0 +1,24 @@
+
+package edu.eci.cosw.cheapestPrice.exception;
+
+/**
+ * Created by Daniela Sepulveda.
+ */
+public class CheapestPriceException extends Exception{
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public CheapestPriceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * @param cause
+     */
+    public CheapestPriceException(String cause) {
+
+        super(cause);
+    }
+}
