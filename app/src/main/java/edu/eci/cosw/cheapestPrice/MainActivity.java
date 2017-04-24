72b6efb1d69da6d2700b1a2f44eb217926576bcf
Julian David Devia Serna <judadese@gmail.com>
2017-04-24 17:57:44 -0500

    hay pantalla inicial

--- app/src/main/java/edu/eci/cosw/cheapestPrice/MainActivity.java
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/MainActivity.java
@@ -1,13 +1,29 @@
-package co.edu.cosw.cheapestprice;
+package edu.eci.cosw.cheapestPrice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email= (EditText) findViewById(R.id.email);
+        password= (EditText) findViewById(R.id.password);
+        login= (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
