7eaf53553d01ea859b136bef730953f9d3b59a53
Julian David Devia Serna <judadese@gmail.com>
2017-04-24 18:10:53 -0500

    inicio de login

--- app/src/main/java/edu/eci/cosw/cheapestPrice/MainActivity.java
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText email,password;
@@ -20,9 +23,38 @@ public class MainActivity extends AppCompatActivity {
         password= (EditText) findViewById(R.id.password);
         login= (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
+            /*
+            código de angular que se debe "traducir"(btoa encripta en base-64):
+            var headers = credentials ? {authorization: "Basic "
+                            + btoa(credentials.username + ":" + credentials.password)
+                } : {};
+
+                $http.get('user', {headers: headers}).then(successCallback, errorCallback);
+
+                function successCallback(data){
+                    if (data.data.name) {
+                        $rootScope.authenticated = true;
+                    }else {
+                        $rootScope.authenticated = false;
+                    }
+                        callback && callback();
+                }
+
+                function errorCallback(error){
+                    $rootScope.authenticated = false;
+                     callback && callback();
+                }
+             */
             @Override
             public void onClick(View v) {
+                /*ExecutorService executorService = Executors.newFixedThreadPool(1);
 
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        Se necesita una RetrofitNetwork, un RequestCallback, y un UserLoginService (creo) y buscar como convertir a base-64 para copiar el get que se hace en angular
+                    }
+                });*/
             }
         });
     }
