e542c4141a3338d2483e726f88631b35b9d86c5f
German Lopez <germaniuslp@hotmail.com>
2017-04-25 09:46:06 -0500

    Empezando con las listas de mercado

---
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/ShoppingListActivity.java
@@ -0,0 +1,15 @@
+package edu.eci.cosw.cheapestPrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.eci.cosw.cheapestPrice.R;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
