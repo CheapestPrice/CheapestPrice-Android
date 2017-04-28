356347be6dce7154c715e2c25ebdc3630a7da36a
Julian Devia <judadese@gmail.com>
2017-04-28 17:11:17 -0500

    desvío para probar, dirige a diferentes vistas a clientes y a tenderos

--- app/src/main/java/edu/eci/cosw/cheapestPrice/MainActivity.java
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/MainActivity.java
@@ -3,10 +3,15 @@ package edu.eci.cosw.cheapestPrice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.util.Base64;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import java.security.Principal;
 import java.util.concurrent.ExecutorService;
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     Button login;
     Button register;
     RetrofitNetwork network;
+    SwitchCompat select;
 
 
     @Override
@@ -35,8 +41,22 @@ public class MainActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.login);
         register = (Button) findViewById(R.id.register);
         network = new RetrofitNetwork();
-        login.setOnClickListener(new View.OnClickListener() {
-            /*
+        select= (SwitchCompat) findViewById(R.id.selectUser);
+        select.setTextOff("Cliente");
+        select.setTextOn("Tendero");
+        select.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                System.out.println(select.isActivated());
+                System.out.println(select.isChecked());
+                if(isChecked){
+                    select.setText(select.getTextOn());
+                }else{
+                    select.setText(select.getTextOff());
+                }
+            }
+        });
+        /*login.setOnClickListener(new View.OnClickListener() {
             código de angular que se debe "traducir"(btoa encripta en base-64):
             var headers = credentials ? {authorization: "Basic "
                             + btoa(credentials.username + ":" + credentials.password)
@@ -57,11 +77,9 @@ public class MainActivity extends AppCompatActivity {
                     $rootScope.authenticated = false;
                      callback && callback();
                 }
-             */
             @Override
             public void onClick(View v) {
                 ExecutorService executorService = Executors.newFixedThreadPool(1);
-
                 executorService.execute(new Runnable() {
                     @Override
                     public void run() {
@@ -85,9 +103,42 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-        });
+        });*/
 
     }
+    public void logIn(View view){
+        if(select.getText().equals(select.getTextOn())){
+            Intent intent= new Intent(this,ProductActivity.class);
+            startActivity(intent);
+        }else{
+            Intent intent= new Intent(this,SearchActivity.class);
+            startActivity(intent);
+        }
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                //Se necesita una RetrofitNetwork, un RequestCallback, y un UserLoginService (creo) y buscar como convertir a base-64 para copiar el get que se hace en angular
+                byte[] encodedBytes = Base64.encode((email.getText().toString() + ":" + password.getText().toString()).getBytes(), Base64.DEFAULT);
+                String cred = new String(encodedBytes);
+                System.out.println(email.getText().toString() + ":" + password.getText().toString());
+                System.out.println(cred);
+                network.getPrincipal(new RequestCallback<Principal>() {
+                    @Override
+                    public void onSuccess(Principal response) {
+                        System.out.println(response);
+                    }
+
+                    @Override
+                    public void onFailed(NetworkException e) {
+                        System.out.println("La tarea fallo exitosamente");
+                        System.out.println(e);
+                    }
+                },new User(new Headers("Basic "+cred)));
+            }
+        });
+    }
+
     public void reg(View view){
         Intent intent= new Intent(this,RegisterActivity.class);
         startActivity(intent);
