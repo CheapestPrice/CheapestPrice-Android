798e6c600d3c222ba7e8ec28ddc5f5b05f220030
Julian Devia <judadese@gmail.com>
2017-04-28 14:43:47 -0500

    validación de formulario de registro completada, ya se puede implementar el post para registrar

--- app/src/main/java/edu/eci/cosw/cheapestPrice/RegisterActivity.java
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/RegisterActivity.java
@@ -1,8 +1,10 @@
 package edu.eci.cosw.cheapestPrice;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -47,37 +49,83 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void reg(View v) {
-        if(userName.getText()==null || userName.getText().length()==0) {
-            userName.setError("Falta el nombre de usuario");
-            System.out.println("Falta el nombre de usuario");
-        }
-        if(email.getText()==null || email.getText().length()==0){
-            email.setError("Falta el correo electrónico");
-            System.out.println("Falta el correo electrónico");
-        }
-        if(!isEmailValid(email.getText().toString())){
-            email.setError("Formato incorrecto del correo electrónico");
-            System.out.println("Formato incorrecto del correo electrónico");
-        }
-        if(passwd.getText()==null || passwd.getText().length()==0) {
-            passwd.setError("Falta la contraseña");
-            System.out.println("Falta la contraseña");
-        }
-        if(confirmPasswd.getText()==null || confirmPasswd.getText().length()==0){
-            confirmPasswd.setError("Falta la confirmación de contraseña");
-            System.out.println("Falta la confirmación de contraseña");
-        }
-        if( !passwd.getText().toString().equals(confirmPasswd.getText().toString())){
-            confirmPasswd.setError("Las contraseñas no coinciden");
-            System.out.println("las contraseñas no son iguales");
-        }
-        if(isTendero){
-            if(shopAddress.getText()==null || shopAddress.getText().length()==0){
-                shopAddress.setError("La dirección de la tienda no puede estar vacía");
-                System.out.println("La dirección de la tienda no puede estar vacía");
+        System.out.println(validForm());
+        if(!validForm()) {
+            if (userName.getText() == null || userName.getText().length() == 0) {
+                userName.setError("Falta el nombre de usuario");
+                System.out.println("Falta el nombre de usuario");
             }
+            if (email.getText() == null || email.getText().length() == 0) {
+                email.setError("Falta el correo electrónico");
+                System.out.println("Falta el correo electrónico");
+            }
+            if (!isEmailValid(email.getText().toString())) {
+                email.setError("Formato incorrecto del correo electrónico");
+                System.out.println("Formato incorrecto del correo electrónico");
+            }
+            if (passwd.getText() == null || passwd.getText().length() == 0) {
+                passwd.setError("Falta la contraseña");
+                System.out.println("Falta la contraseña");
+            }
+            if (confirmPasswd.getText() == null || confirmPasswd.getText().length() == 0) {
+                confirmPasswd.setError("Falta la confirmación de contraseña");
+                System.out.println("Falta la confirmación de contraseña");
+            }
+            if (!passwd.getText().toString().equals(confirmPasswd.getText().toString())) {
+                confirmPasswd.setError("Las contraseñas no coinciden");
+                System.out.println("las contraseñas no son iguales");
+            }
+            if (isTendero) {
+                if (shopAddress.getText() == null || shopAddress.getText().length() == 0) {
+                    shopAddress.setError("La dirección de la tienda no puede estar vacía");
+                    System.out.println("La dirección de la tienda no puede estar vacía");
+                }
+                if (shopName.getText() == null || shopName.getText().length() == 0) {
+                    shopName.setError("El nombre de la tienda no puede estar vacío");
+                    System.out.println("El nombre de la tienda no puede estar vacío");
+                }
+                if (shopNit.getText() == null || shopNit.getText().length() == 0) {
+                    shopNit.setError("El NIT de la tienda no puede estar vacío");
+                    System.out.println("El NIT de la tienda no puede estar vacío");
+                }
+                if (shopPhone.getText() == null || shopPhone.getText().length() == 0) {
+                    shopPhone.setError("El telefono de la tienda no puede estar vacío");
+                    System.out.println("El telefono de la tienda no puede estar vacío");
+                }
+            }
+        }else{
+            System.out.println("validoooooo");
+            alertDialog("Registro exitoso");
         }
 
+    }
+
+    public void alertDialog(String e) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(e)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private boolean validForm(){
+        boolean ans=true;
+        ans &= !(userName.getText()==null || userName.getText().length()==0) ;
+        ans &= !(email.getText()==null || email.getText().length()==0) ;
+        ans &= !(email.getText()==null || email.getText().length()==0) && isEmailValid(email.getText().toString());
+        ans &= !(passwd.getText()==null || passwd.getText().length()==0);
+        ans &= !(confirmPasswd.getText()==null || confirmPasswd.getText().length()==0);
+        ans &= passwd.getText().toString().equals(confirmPasswd.getText().toString());
+        ans &= ((isTendero == !(shopAddress.getText()==null || shopAddress.getText().length()==0)) || !isTendero);
+        ans &= ((isTendero == !(shopName.getText()==null || shopName.getText().length()==0)) || !isTendero);
+        ans &= ((isTendero == !(shopNit.getText()==null || shopNit.getText().length()==0)) || !isTendero);
+        ans &= ((isTendero == !(shopPhone.getText()==null || shopPhone.getText().length()==0)) || !isTendero);
+        return ans;
     }
     private boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
