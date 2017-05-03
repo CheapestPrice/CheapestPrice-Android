260a29479c832edc818792a7a5a8ed54ef14d8bc
Daniela <daniela.sepulveda@mail.escuelaing.edu.co>
2017-05-03 10:02:16 -0500

    items

---
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/DetalleProductActivity.java
@@ -0,0 +1,14 @@
+package edu.eci.cosw.cheapestPrice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class DetalleProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_product);
+    }
+}
--- app/src/main/java/edu/eci/cosw/cheapestPrice/ProductActivity.java
+++ app/src/main/java/edu/eci/cosw/cheapestPrice/ProductActivity.java
@@ -1,9 +1,11 @@
 package edu.eci.cosw.cheapestPrice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -55,6 +57,10 @@ public class ProductActivity extends AppCompatActivity {
             }
 
         });
+
+        /*
+        Bundle extras = getIntent().getExtras();
+        Item p= (Item) extras.get("item");*/
     }
 
     private void configureRecyclerView() {
@@ -65,6 +71,26 @@ public class ProductActivity extends AppCompatActivity {
     }
 
     private void refresh(){
-        recyclerView.setAdapter(new ItemsAdapter(items,this));
+        recyclerView.setAdapter(new ItemsAdapter(items, this, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                switch (v.getId()){
+                    case R.id.updateProduct:
+                        int pos=recyclerView.getChildAdapterPosition(v.getRootView());
+                        Item p=items.get(pos);
+                         intent=new Intent(ProductActivity.this,DetalleProductActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("item",p);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    case R.id.deleteProduct:
+                        //Intente actividad eliminar product
+                        intent= new Intent(ProductActivity.this,RegisterActivity.class);
+                        startActivity(intent);
+                }
+
+            }
+        }));
     }
 }
